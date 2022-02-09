1d9344f326a64e570e0be4a407c2851c52487aff
shathish annamalai <shatishsyn@gmail.com>
2022-02-08 21:42:37 -0400

    Boiler plate code

---
+++ src/main/java/com/dalhousie/MealStop/MealStopApplication.java
@@ -0,0 +1,13 @@
+package com.dalhousie.MealStop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealStopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MealStopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dalhousie/MealStop/MealStopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dalhousie.MealStop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealStopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
