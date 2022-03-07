cd1ae5cc7006a499e2b0bca91a344411684074e0
sharad <sh244392.dal.ca>
2022-03-06 21:33:12 -0400

    detabase setup for orders

--- src/main/java/com/dalhousie/MealStop/orders/service/IOrderService.java
+++ src/main/java/com/dalhousie/MealStop/orders/service/IOrderService.java
@@ -5,14 +5,12 @@ import com.dalhousie.MealStop.orders.model.Orders;
 import java.util.List;
 
 public interface IOrderService {
-    public abstract void addOrder(Orders orders);
+    public void addOrder(Orders newOrder);
     public abstract List<Orders> getAllOrders();
 
-    public abstract List<Orders> getOrdersByUserID(int userId);
+    public abstract List<Orders> getOrdersByCustomerID(int userId);
 
     public abstract Orders getOrderByOrderID(int userId);
-
-    public abstract Orders getOrderByUserIDandOrderID(int userId, int orderId);
 
     public abstract List<Orders> getOrdersByRestaurantID(int restaurantId);
 }
