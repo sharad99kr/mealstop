061442f784033af544db1168d9df40ed2c00bfac
kuldeep <kl681090@dal.ca>
2022-03-19 13:29:39 -0300

    NGO interface

--- src/main/java/com/dalhousie/MealStop/Restaurant/repository/RestaurantRepository.java
+++
@@ -1,9 +0,0 @@
-package com.dalhousie.MealStop.Restaurant.repository;
-
-import com.dalhousie.MealStop.Restaurant.model.Restaurant;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
-}
