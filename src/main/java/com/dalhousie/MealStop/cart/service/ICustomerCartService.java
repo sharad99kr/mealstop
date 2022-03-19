7dbde3cc32a6a164b745560247d5a2d10f07e1c4
shathish annamalai <shatishsyn@gmail.com>
2022-03-19 15:44:36 -0300

    Added customer cart controller, service and modal to let user add meals to cart

---
+++ src/main/java/com/dalhousie/MealStop/cart/service/ICustomerCartService.java
@@ -0,0 +1,14 @@
+package com.dalhousie.MealStop.cart.service;
+
+import com.dalhousie.MealStop.Meal.model.Meal;
+import com.dalhousie.MealStop.cart.modal.CustomerCart;
+import com.dalhousie.MealStop.customer.modal.ICustomer;
+
+import java.util.HashMap;
+
+public interface ICustomerCartService
+{
+    public CustomerCart getCustomerCart();
+    public void addMealsToCustomerCart(Meal meal);
+    public void removeMealsFromCustomerCart(Meal meal);
+}
