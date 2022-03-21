b621724867bad2064ee7ccfc14f21c0213796dac
kuldeep <kl681090@dal.ca>
2022-03-21 15:40:17 -0300

    NGO UI

---
+++ src/test/java/com/dalhousie/MealStop/customer/controller/CustomerControllerTest.java
@@ -0,0 +1,12 @@
+package com.dalhousie.MealStop.customer.controller;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.FileNotFoundException;
+
+public class CustomerControllerTest {
+        @Test(excpected = FileNotFoundException.class)
+    public void getLandingPageTest() throws FileNotFoundException{
+        String model
+}
+}
