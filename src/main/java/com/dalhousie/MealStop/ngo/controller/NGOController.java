0927dce72e83626312e6b54a7a61232bea63946d
shathish annamalai <shatishsyn@gmail.com>
2022-03-28 18:46:55 -0300

    added ngo landing page and profile page in controller

--- src/main/java/com/dalhousie/MealStop/ngo/controller/NGOController.java
+++ src/main/java/com/dalhousie/MealStop/ngo/controller/NGOController.java
@@ -1,6 +1,9 @@
 package com.dalhousie.MealStop.ngo.controller;
 
 
+import com.dalhousie.MealStop.customer.modal.Customer;
+import com.dalhousie.MealStop.ngo.modal.NGO;
+import com.dalhousie.MealStop.ngo.service.INGOService;
 import com.dalhousie.MealStop.orders.model.Orders;
 import com.dalhousie.MealStop.orders.service.IOrderService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +19,8 @@ public class NGOController
     @Autowired
     private IOrderService orderService;
 
+    @Autowired INGOService ngoService;
+
     @GetMapping("/ngo/homepage")
     public String getLandingPage(Model model)
     {
@@ -24,4 +29,11 @@ public class NGOController
         return "ngo/landing-page";
     }
 
+    @GetMapping("/ngo/profile")
+    public String getCustomerProfilePage(Model model)
+    {
+        NGO ngoUser = ngoService.getNGODetailsFromSession();
+        model.addAttribute("ngoUser", ngoUser);
+        return "ngo/profile";
+    }
 }
