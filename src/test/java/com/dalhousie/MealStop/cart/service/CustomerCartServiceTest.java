e3f9b7abebd738895aa8ae5a6b3d1df8a1365d9b
kuldeep <kl681090@dal.ca>
2022-03-29 07:29:38 -0300

    CART TEST

---
+++ src/test/java/com/dalhousie/MealStop/cart/service/CustomerCartServiceTest.java
@@ -0,0 +1,48 @@
+package com.dalhousie.MealStop.cart.service;
+
+import com.dalhousie.MealStop.Meal.model.Meal;
+import com.dalhousie.MealStop.cart.modal.CustomerCart;
+import com.dalhousie.MealStop.customer.modal.Customer;
+import com.dalhousie.MealStop.customer.service.ICustomerService;
+import com.dalhousie.MealStop.user.entity.User;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@ExtendWith(MockitoExtension.class)
+class CustomerCartServiceTest {
+    @Mock
+    private ICustomerService customerService;
+
+    @InjectMocks
+    CustomerCartService customerCartService;
+
+    @InjectMocks
+    private HashMap<Long, CustomerCart> mockCustomersCartMap;
+
+    private CustomerCart customerCart;
+    private Meal meal1;
+    private ArrayList<Meal> mealList;
+
+
+}
