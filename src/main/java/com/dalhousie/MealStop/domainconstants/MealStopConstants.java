8c520394554aeef700f40dee51f3976f7aaf9ccc
kuldeep <kl681090@dal.ca>
2022-03-30 15:15:41 -0300

    CONSTANT

--- src/main/java/com/dalhousie/MealStop/domainconstants/MealStopConstants.java
+++ src/main/java/com/dalhousie/MealStop/domainconstants/MealStopConstants.java
@@ -4,8 +4,11 @@ public class MealStopConstants
 {
     public enum UserTypes {
         CUSTOMER,
-        RESTAURANT_OWNER
+        RESTAURANT_OWNER,
+        NGO
     }
 
     public static final Integer CUSTOMER_DEFAULT_TOKENS = 10;
+
+    public static final Integer NGO_DEFAULT_ORDERS = 0;
 }
