075212b17956c50ececb39cc36ffbe5267326f27
kuldeep <kl681090@dal.ca>
2022-03-30 11:38:21 -0300

    NGO CONTROLLER

---
+++ src/main/java/com/dalhousie/MealStop/ngo/NGOController.java
@@ -0,0 +1,32 @@
+package com.dalhousie.MealStop.ngo;
+
+
+import com.dalhousie.MealStop.customer.modal.Customer;
+import com.dalhousie.MealStop.ngo.modal.NGO;
+import com.dalhousie.MealStop.ngo.service.INGOService;
+import com.dalhousie.MealStop.orders.model.Orders;
+import com.dalhousie.MealStop.orders.service.IOrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class NGOController {
+    @Autowired
+    private IOrderService orderService;
+
+    @Autowired
+    INGOService ngoService;
+
+    @GetMapping("/ngo/homepage")
+    public String getLandingPage(Model model) {
+        List<Orders> orders = orderService.getAllCanceledOrders();
+        model.addAttribute("orders", orders);
+        return "ngo/landing-page";
+    }
+
+
+}
