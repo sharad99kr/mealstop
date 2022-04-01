ec67e1401f3910bb3d91499290a0623ae89f5892
sharad <sh244392.dal.ca>
2022-04-01 16:17:54 -0300

    test case in progress

--- src/test/java/com/dalhousie/MealStop/Reward/service/RewardServiceTest.java
+++ src/test/java/com/dalhousie/MealStop/Reward/service/RewardServiceTest.java
@@ -1,14 +1,23 @@
 package com.dalhousie.MealStop.Reward.service;
 
+import com.dalhousie.MealStop.Reward.constants.Constants;
 import com.dalhousie.MealStop.Reward.model.Rewards;
 import com.dalhousie.MealStop.Reward.repository.RewardRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+
+import static org.mockito.Mockito.doNothing;
+import org.mockito.internal.stubbing.answers.DoesNothing;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -20,26 +29,28 @@ class RewardServiceTest {
     @Mock
     private RewardRepository mockRewardRepository;
 
-    @Mock
-    private static IRewardService rewardService;
 
 
-    private Long mockCustomerId;
 
     @Mock
     private Rewards mockRewards;
 
 
     private int mockRewardPoint;
+    private long mockCustomerId;
 
+
+    @Autowired
+    @Mock
+    private static RewardService rewardService;
 
     @BeforeEach
     public void setup() {
-
+        mockCustomerId=1L;
         mockRewards=new Rewards(Long.valueOf(123),101);
         mockRewardPoint=101;
         MockitoAnnotations.initMocks(this);
-        rewardService=new RewardService();
+
 
     }
 
@@ -52,10 +63,10 @@ class RewardServiceTest {
 
     @Test
     void getRewardPointsWithRewards() {
-        MockitoAnnotations.initMocks(this);
-        mockCustomerId= Long.valueOf(123);
-        when(rewardService.getRewardPoints(mockCustomerId)).thenReturn( mockRewardPoint);
-        assertEquals(rewardService.getRewardPoints(mockCustomerId),101);
+//        MockitoAnnotations.initMocks(this);
+//        mockCustomerId= Long.valueOf(123);
+//        when(rewardService.getRewardPoints(mockCustomerId)).thenReturn( mockRewardPoint);
+//        assertEquals(rewardService.getRewardPoints(mockCustomerId),101);
 
     }
 //
@@ -65,15 +76,36 @@ class RewardServiceTest {
 //
 //    @Test
 //    void resetRewardPoints() {
-//    }
 //
+//        doNothing().
+//                doThrow(new RuntimeException())
+//                .when(mockRewardRepository).updateRewardsById(mockCustomerId,0);
+//        doNothing().
+//                doThrow(new RuntimeException())
+//                .when(rewardService).updateRewardPoints(mockCustomerId,0);
+//        rewardService.updateRewardPoints(mockCustomerId, 0);
+//
+//        verify(mockRewardRepository,times(1)).updateRewardsById(mockCustomerId,0);
+//    }
+
 //    @Test
 //    void updateRewardPoints() {
-//    }
 //
-//    @Test
-//    void isRewardPointsRedeemable() {
+//        doNothing().
+//                doThrow(new RuntimeException())
+//                .when(mockRewardRepository).updateRewardsById(mockCustomerId,10);
+//
+//
+//        rewardService.updateRewardPoints(mockCustomerId, 10);
+//        verify(mockRewardRepository,times(1)).updateRewardsById(mockCustomerId,10);
+//
 //    }
+
+    @Test
+    void isRewardPointsRedeemable() {
+
+
+    }
 //
 //    @Test
 //    void redeemRewardPoints() {
