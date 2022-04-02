0aecaa6940746528852214cf203457b889ca7913
shathish annamalai <shatishsyn@gmail.com>
2022-04-02 19:39:07 -0300

    added test for main method and removed smells

--- src/test/java/com/dalhousie/MealStop/MealStopApplicationTests.java
+++ src/test/java/com/dalhousie/MealStop/MealStopApplicationTests.java
@@ -1,13 +1,20 @@
 package com.dalhousie.MealStop;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.mockito.Mockito.*;
+
 @SpringBootTest
-class MealStopApplicationTests {
-
-	/*@Test
-	void contextLoads() {
-	}*/
-
+class MealStopApplicationTests
+{
+    @Test
+    void testMain()
+    {
+        MealStopApplication application = Mockito.mock(MealStopApplication.class);
+        String [] args = { "one", "two", "three" };
+        application.main(args);
+        verifyZeroInteractions(application);
+    }
 }
