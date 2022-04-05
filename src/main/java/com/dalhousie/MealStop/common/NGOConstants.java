6f15160a18f1db95c37748a236855f2c474536a2
shathish annamalai <shatishsyn@gmail.com>
2022-04-05 14:20:29 -0300

    added new test for NGO

--- src/main/java/com/dalhousie/MealStop/common/NGOConstants.java
+++ src/main/java/com/dalhousie/MealStop/common/NGOConstants.java
@@ -2,6 +2,7 @@ package com.dalhousie.MealStop.common;
 public class NGOConstants
 {
     public static final String NGO_NOTIFICATION_SUBJECT = "Cancelled meal available for pickup";
+
     public static String getNgoNotificationContent(String ngoName, String mealName)
     {
         StringBuilder stringBuilder = new StringBuilder();
